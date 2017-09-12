95560250f26cbecbc02cae218a9fa6021b5aa70c
Fulton Zink <joeyzink@gmail.com>
2017-09-12 12:41:48 -0700

    Revert "Add RegistrationForm from previous repository and add screen in StackNavigator in App"
    This reverts commit 7040fab71e03d228099624f7b70960e2e92e8577.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { StackNavigator } from 'react-navigation';
 import MapScreen from './components/MapScreen';
 import LoginForm from './components/LoginForm';
-import RegistrationForm from './components/RegistrationForm';
 
 const App = StackNavigator({
   Login: { screen: LoginForm },
-  Map: { screen: MapScreen },
-  Register: { screen: RegistrationForm }
+  Map: { screen: MapScreen }
 });
 
 export default App;
