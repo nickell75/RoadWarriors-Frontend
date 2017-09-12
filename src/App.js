9f8e4d04b7740f8c1ece87e3c1f9649c0710ef70
Fulton Zink <joeyzink@gmail.com>
2017-09-12 15:11:36 -0700

    Add navigation to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { StackNavigator } from 'react-navigation';
 import MapScreen from './components/MapScreen';
 import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
 
 const App = StackNavigator({
   Login: { screen: LoginForm },
-  Map: { screen: MapScreen }
+  Map: { screen: MapScreen },
+  Register: { screen: RegistrationForm }
 });
 
 export default App;
